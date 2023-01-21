87b310c4dc91b3db398337d601dcb79a9bbca833
vikkitorry <vikkitorry6716@gmail.com>
2023-01-21 17:06:47 +1000

    Third commit (till weather, async)

--- js/script.js
+++ js/script.js
@@ -71,17 +71,58 @@ let randomNum = getRandomNum();
 const setBg = () => {
     const timeOfDate = getTimeOfDay();
     const bgNum = randomNum.toString().padStart(2, '0');
-    body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDate}/${bgNum}.jpg')`;
+//smooth change BackGround Image
+    const img = new Image();
+    img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDate}/${bgNum}.jpg`;
+    img.onload = () => {
+        body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDate}/${bgNum}.jpg')`;
+    }
 }
 
 setBg()
 
-//slaider
+//Slaider change BackGround Image
 
-const getSlideNext = () => {
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
 
+function getSlideNext() {
+     randomNum < 20 ? (randomNum++) : (randomNum = 1);
+     setBg() 
+}
+function getSlidePrev() {
+    randomNum > 1 ? (randomNum--) : (randomNum = 20);
+    setBg() 
 }
 
+slideNext.addEventListener('click', getSlideNext);
+slidePrev.addEventListener('click', getSlidePrev);
+
+// weather
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
+const weatherDescription = document.querySelector('.weather-description');
+const city = document.querySelector('.city');
+
+async function getWeather() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=313445949dd6e620b03c5721573a7c81&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${data.main.temp}°C`;
+
+    wind.textContent = `${data.wind.speed}`;
+    humidity.textContent = `${data.main.humidity}`;
+    
+    weatherDescription.textContent = data.weather[0].description;}
+getWeather()
+
+city.addEventListener('change', getWeather)
+
+//
 
 
 
