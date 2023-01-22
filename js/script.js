4f2faf8e7095b1471e2f13fa5a884f3b291d5cc6
vikkitorry <vikkitorry6716@gmail.com>
2023-01-22 14:00:59 +1000

    Eight commit (audio)

--- js/script.js
+++ js/script.js
@@ -78,8 +78,7 @@ const setBg = () => {
         body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDate}/${bgNum}.jpg')`;
     }
 }
-
-setBg()
+setBg();
 
 //Slaider change BackGround Image
 
@@ -99,12 +98,14 @@ slideNext.addEventListener('click', getSlideNext);
 slidePrev.addEventListener('click', getSlidePrev);
 
 // weather
+
 const weatherIcon = document.querySelector('.weather-icon');
 const temperature = document.querySelector('.temperature');
 const wind = document.querySelector('.wind');
 const humidity = document.querySelector('.humidity');
 const weatherDescription = document.querySelector('.weather-description');
 const city = document.querySelector('.city');
+city.value = 'Krasnoyarsk';
 
 async function getWeather() {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=313445949dd6e620b03c5721573a7c81&units=metric`;
@@ -112,18 +113,18 @@ async function getWeather() {
     const data = await res.json();
     weatherIcon.className = 'weather-icon owf';
     weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-    temperature.textContent = `${data.main.temp}°C`;
+    temperature.textContent = `${Math.round(data.main.temp)}°C`;
 
-    wind.textContent = `${data.wind.speed}`;
-    humidity.textContent = `${data.main.humidity}`;
+    wind.textContent = `Wind speed: ${Math.round(data.wind.speed)} m/s`;
+    humidity.textContent = `Humidity: ${data.main.humidity}%`;
     
     weatherDescription.textContent = data.weather[0].description;
 }
 getWeather()
-
-city.addEventListener('change', getWeather)
+city.addEventListener('change', getWeather);
 
 // quotes
+
 const quote = document.querySelector('.quote');
 const author = document.querySelector('.author');
 const changeQuote = document.querySelector('.change-quote');
@@ -136,12 +137,43 @@ async function getQuotes() {
     quote.textContent = data[rundomQuote].text;
     author.textContent = data[rundomQuote].author;
   }
-  getQuotes()
-changeQuote.addEventListener('click', getQuotes)
+getQuotes()
+changeQuote.addEventListener('click', getQuotes);
 
-//
+//audio
 
+const play = document.querySelector('.play');
+const playPrevBtn = document.querySelector('.play-prev');
+const playNextBtn = document.querySelector('.play-next');
+let isPlay = false;
+const audio = new Audio();
+let playNum = 0;
 
+function playAudio() {
+    if (!isPlay) {
+        audio.src = '';
+        audio.currentTime = 0;
+        audio.play();
+        play.classList.toggle('pause');
+    } else if (isPlay == false) {
+        play.classList.toggle('pause');
+        audio.pause();
+    }
+}
+
+function playPrev() {
+    playNum = 0 ? playNum = 4 : playNum--
+    playAudio()
+}
+
+function playNext() {
+    playNum = 4 ? playNum = 0 : playNum++
+    playAudio()
+}
+
+play.addEventListener('click', playAudio);
+playPrevBtn.addEventListener('click', playPrev);
+playNextBtn.addEventListener('click', playNext);
 
 
 
